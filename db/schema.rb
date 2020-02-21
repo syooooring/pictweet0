6580870e76909aecf92cb4e2193f72ca3e323fd1
syooooring <syooooring@gmail.com>
2020-02-21 20:16:54 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_20_121023) do
+ActiveRecord::Schema.define(version: 2020_02_21_111613) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.string "text"
     t.text "image"
     t.datetime "created_at", null: false
